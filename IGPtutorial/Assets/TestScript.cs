a2c8995cf1e934877edea8493a8b6d539f249682
LUCENA <mmarti206@lab-m627-osx-17.local>
2014-10-03 13:46:36 +0100

    testBranch changes

--- IGPtutorial/Assets/TestScript.cs
+++ IGPtutorial/Assets/TestScript.cs
@@ -1,10 +1,11 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class TestScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		print(“Hello World”);
 	
 	}
 	
